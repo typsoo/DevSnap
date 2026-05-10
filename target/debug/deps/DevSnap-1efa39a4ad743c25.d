6876762926b67b6c8c3d45abe05ac0d0b11d8897
typsoo <ilya.paliashchuk@gmail.com>
2026-05-10 16:37:48 +0200

    Initial commit

---
+++ target/debug/deps/DevSnap-1efa39a4ad743c25.d
@@ -0,0 +1,5 @@
+/home/typsoo/Projects/RUST/DevSnap/target/debug/deps/DevSnap-1efa39a4ad743c25.d: src/main.rs
+
+/home/typsoo/Projects/RUST/DevSnap/target/debug/deps/libDevSnap-1efa39a4ad743c25.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/DevSnap-5cdf8f3abccc8bfd.d
@@ -0,0 +1,5 @@
+/home/typsoo/Projects/RUST/DevSnap/target/debug/deps/DevSnap-5cdf8f3abccc8bfd.d: src/main.rs
+
+/home/typsoo/Projects/RUST/DevSnap/target/debug/deps/libDevSnap-5cdf8f3abccc8bfd.rmeta: src/main.rs
+
+src/main.rs:
